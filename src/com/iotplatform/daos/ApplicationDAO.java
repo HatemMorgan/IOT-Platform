14e7fd55f6008bbadd5a6338a99e40d9113ff56f
Hatem Morgan <hatemmorgan17@gmail.com>
2017-02-18 12:43:53 +0200

    Adding methods to create and drop application graph models from database and complete adding testing.

--- src/com/iotplatform/daos/ApplicationDAO.java
+++ src/com/iotplatform/daos/ApplicationDAO.java
@@ -8,44 +8,41 @@ import java.sql.Statement;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.jena.rdf.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Component;
 
+import oracle.spatial.rdf.client.jena.ModelOracleSem;
+import oracle.spatial.rdf.client.jena.Oracle;
+import oracle.spatial.rdf.client.jena.OracleUtils;
+
 /*
  *  Application Data Access Object which is responsible for dealing with application graph in database
  */
 @Component
-public class ApplicationDAO extends JdbcDaoSupport {
-	private JdbcTemplate jdbcTemplate;
+public class ApplicationDAO {
+	private Oracle oracle;
+	private final String suffix = "_MODEL";
 
 	@Autowired
-	public ApplicationDAO(DataSource dataSource) {
+	public ApplicationDAO(Oracle oracle) {
 		System.out.println("ApplicationDAO Created");
-		this.setDataSource(dataSource);
-		this.jdbcTemplate = this.getJdbcTemplate();
+		this.oracle = oracle;
 	}
 
 	public boolean checkIfApplicationModelExsist(String applicationName) {
 
 		try {
+			String modelConventionName = applicationName.toUpperCase() + suffix;
 			String queryString = "SELECT COUNT(*) FROM  MDSYS.SEM_MODEL$ WHERE MODEL_NAME= ? ";
-			PreparedStatement stat = jdbcTemplate.getDataSource().getConnection().prepareStatement(queryString);
-			stat.setString(1, applicationName);
+
+			PreparedStatement stat = oracle.getConnection().prepareStatement(queryString);
+			stat.setString(1, modelConventionName);
 			ResultSet resultSet = stat.executeQuery();
 			resultSet.next();
 			int result = resultSet.getInt(1);
-			System.out.println(result);
-
-			/*
-			 * testing using oracle insted of jdbc and it works the same stat=
-			 * oracle.getConnection().prepareStatement(queryString);
-			 * stat.setString(1, applicationName); resultSet =
-			 * stat.executeQuery(); resultSet.next(); result =
-			 * resultSet.getInt(1); System.out.println(result);
-			 */
-
 			if (result == 1) {
 				return true;
 			} else {
@@ -58,24 +55,58 @@ public class ApplicationDAO extends JdbcDaoSupport {
 		return false;
 
 	}
-	
-	public boolean createNewApplicationModel(String applicationName){
-		
+
+	/*
+	 * createNewApplicationModel method creates a new application model in the
+	 * database that will hold the application data represented in a graph
+	 */
+	public boolean createNewApplicationModel(String applicationName) {
+		try {
+			String modelConventionName = applicationName.toUpperCase() + suffix;
+			Model newModel = ModelOracleSem.createOracleSemModel(oracle, modelConventionName);
+			newModel.close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	/*
+	 * dropApplicationModel method drops an application model and removes all
+	 * the data associated with this model
+	 */
+	public boolean dropApplicationModel(String applicationName) {
+		try {
+			String modelConventionName = applicationName.toUpperCase() + suffix;
+			OracleUtils.dropSemanticModel(oracle, modelConventionName);
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 //	public static void main(String[] args) {
-//		String szJdbcDriver = "oracle.jdbc.driver.OracleDriver";
 //		String szJdbcURL = "jdbc:oracle:thin:@127.0.0.1:1539:cdb1";
 //		String szUser = "rdfusr";
 //		String szPasswd = "rdfusr";
-//		BasicDataSource dataSource = new BasicDataSource();
-//		dataSource.setDriverClassName(szJdbcDriver);
-//		dataSource.setUrl(szJdbcURL);
-//		dataSource.setUsername(szUser);
-//		dataSource.setPassword(szPasswd);
+//		
+//		// String szJdbcDriver = "oracle.jdbc.driver.OracleDriver";
+//		// BasicDataSource dataSource = new BasicDataSource();
+//		// dataSource.setDriverClassName(szJdbcDriver);
+//		// dataSource.setUrl(szJdbcURL);
+//		// dataSource.setUsername(szUser);
+//		// dataSource.setPassword(szPasswd);
 //
-//		ApplicationDAO applicationDAO = new ApplicationDAO(dataSource);
-//		applicationDAO.checkIfApplicationModelExsist("TEST");
+//		ApplicationDAO applicationDAO = new ApplicationDAO(new Oracle(szJdbcURL, szUser, szPasswd));
+//		System.out.println("Application Found :"+applicationDAO.checkIfApplicationModelExsist("test"));
+//		System.out.println("Application Created : "+applicationDAO.createNewApplicationModel("test"));
+//		System.out.println("Application Found :"+applicationDAO.checkIfApplicationModelExsist("test"));
+//		System.out.println("Application Drooped : "+applicationDAO.dropApplicationModel("test"));
+//		System.out.println("Application Found :"+applicationDAO.checkIfApplicationModelExsist("test"));
+//
 //	}
 }
