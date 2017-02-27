36088a8b9530d3cbd3c380c69a9248007a1daae4
Hatem Morgan <hatemmorgan17@gmail.com>
2017-02-27 08:06:49 +0200

    done with admin controller class

---
+++ src/com/iotplatform/controllers/AdminController.java
@@ -0,0 +1,34 @@
+package com.iotplatform.controllers;
+
+import java.util.Hashtable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iotplatform.services.AdminService;
+
+@RestController
+public class AdminController {
+
+	@Autowired
+	AdminService adminService;
+
+	@RequestMapping(value = "/admin/{applicationNameCode}", method = RequestMethod.POST)
+	public Hashtable<String, Object> insertNewAdmin(@PathVariable("applicationNameCode") String applicationNameCode,
+			@RequestBody Hashtable<String, Object> htblPropValue) {
+		Hashtable<String, Object> responseJSON = adminService.insertAdmin(htblPropValue, applicationNameCode);
+		return responseJSON;
+	}
+
+	@RequestMapping(value = "/admin/{applicationNameCode}", method = RequestMethod.GET, produces = "application/json")
+	public Hashtable<String, Object> getAdmins(
+			@PathVariable("applicationNameCode") String applicationNameCode) {
+		
+		return adminService.getAdmins(applicationNameCode).getJson();
+	}
+	
+}
--- src/com/iotplatform/controllers/DeveloperController.java
+++ src/com/iotplatform/controllers/DeveloperController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.iotplatform.models.SuccessfullSelectAllJsonModel;
 import com.iotplatform.services.DeveloperService;
 
 @RestController
