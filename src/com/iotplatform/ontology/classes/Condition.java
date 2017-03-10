8e6bb15f89083e27e409c77aec27c7fba264b515
Hatem Morgan <hatemmorgan17@gmail.com>
2017-03-10 20:09:51 +0200

    adding properties to condition and adding static instance to it

--- src/com/iotplatform/ontology/classes/Condition.java
+++ src/com/iotplatform/ontology/classes/Condition.java
@@ -2,7 +2,9 @@ package com.iotplatform.ontology.classes;
 
 import org.springframework.stereotype.Component;
 
+import com.iotplatform.ontology.DataTypeProperty;
 import com.iotplatform.ontology.Prefixes;
+import com.iotplatform.ontology.XSDDataTypes;
 
 /*
  *  This Class maps ssn:Condition class in the ontology
@@ -16,12 +18,44 @@ import com.iotplatform.ontology.Prefixes;
 @Component
 public class Condition extends Property {
 
+	private static Condition conditionInstance;
+
 	public Condition() {
 		super("Condition", "http://purl.oclc.org/NET/ssnx/ssn#Condition", Prefixes.SSN);
 		init();
 	}
 
+	/*
+	 * String nothing parameter is added for overloading constructor technique
+	 * because I need to initialize an instance without having properties and it
+	 * will be always passed by null
+	 */
+	public Condition(String nothing) {
+		super("Condition", "http://purl.oclc.org/NET/ssnx/ssn#Condition", Prefixes.SSN);
+	}
+
+	public static Condition getConditionInstance() {
+		if (conditionInstance == null)
+			conditionInstance = new Condition(null);
+
+		return conditionInstance;
+	}
+
 	private void init() {
+		/*
+		 * Unique Identifier
+		 */
+		super.getProperties().put("id",
+				new DataTypeProperty("id", Prefixes.IOT_LITE, XSDDataTypes.string_typed, false, true));
+
+		/*
+		 * Condition description
+		 */
+		super.getProperties().put("description",
+				new DataTypeProperty("description", Prefixes.IOT_PLATFORM, XSDDataTypes.string_typed, false, false));
+
+		super.getHtblPropUriName().put(Prefixes.IOT_LITE.getUri() + "id", "id");
+		super.getHtblPropUriName().put(Prefixes.IOT_PLATFORM.getUri() + "description", "description");
 
 	}
 }
