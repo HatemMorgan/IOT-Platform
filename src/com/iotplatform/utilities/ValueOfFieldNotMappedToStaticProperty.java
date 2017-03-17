a4d7e02ffd439f224f726ce81ad5fd8ef223586a
Hatem Morgan <hatemmorgan17@gmail.com>
2017-03-17 13:13:12 +0200

    done with creating ValueOfFieldNotMappedToStaticProperty class

--- src/com/iotplatform/utilities/ValueOfFieldNotMappedToStaticProperty.java
+++ src/com/iotplatform/utilities/ValueOfFieldNotMappedToStaticProperty.java
@@ -1,5 +1,7 @@
 package com.iotplatform.utilities;
 
+import com.iotplatform.ontology.Class;
+
 /*
  * ValueOfFieldNotMappedToStaticProperty class is used to create instance that holds the value of the not mapped
  * field to a static property and waiting to be checked after loading dynamic properties 
@@ -13,10 +15,33 @@ public class ValueOfFieldNotMappedToStaticProperty {
 	 * being loaded)
 	 */
 	Class propertyClass;
-	
+
 	/*
-	 * 
+	 * value of the notMappedField
 	 */
 	Object propertyValue;
+
+	/*
+	 * index of instance in the propertyClass instancesList
+	 */
 	int classInstanceIndex;
+
+	public ValueOfFieldNotMappedToStaticProperty(Class propertyClass, Object propertyValue, int classInstanceIndex) {
+		this.propertyClass = propertyClass;
+		this.propertyValue = propertyValue;
+		this.classInstanceIndex = classInstanceIndex;
+	}
+
+	public Class getPropertyClass() {
+		return propertyClass;
+	}
+
+	public Object getPropertyValue() {
+		return propertyValue;
+	}
+
+	public int getClassInstanceIndex() {
+		return classInstanceIndex;
+	}
+
 }
