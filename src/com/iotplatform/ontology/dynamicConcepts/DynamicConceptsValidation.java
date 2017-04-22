b0102cfd9c98e8fdb22f705640066d6249b13e9e
Hatem Morgan <hatemmorgan17@gmail.com>
2017-04-22 09:56:01 +0200

    setting structure and add getPrefix method to get prefix alias in DynamicOntologyRequestValidation

--- src/com/iotplatform/ontology/dynamicConcepts/DynamicConceptsValidation.java
+++ src/com/iotplatform/ontology/dynamicConcepts/DynamicConceptsValidation.java
@@ -1,6 +1,6 @@
 package com.iotplatform.ontology.dynamicConcepts;
 
-import com.iotplatform.exceptions.InvalidDynamicConceptException;
+import com.iotplatform.exceptions.InvalidDynamicOntologyException;
 import com.iotplatform.models.DynamicConceptModel;
 import com.iotplatform.ontology.Prefix;
 import com.iotplatform.ontology.PropertyType;
@@ -43,35 +43,35 @@ public class DynamicConceptsValidation {
 		}
 
 		if (!(newConcept.getIsUnique() == 0 || newConcept.getIsUnique() == 1)) {
-			throw new InvalidDynamicConceptException(
+			throw new InvalidDynamicOntologyException(
 					"Invalid isUnique field value. isUnique field must be 0(false) or 1(true)");
 		}
 
 		if (!(newConcept.getHasMultipleValues() == 0 || newConcept.getHasMultipleValues() == 1)) {
-			throw new InvalidDynamicConceptException(
+			throw new InvalidDynamicOntologyException(
 					"Invalid hasMultipleValues field value. hasMultipleValues field must be 0(false) or 1(true)");
 		}
 
 		if (!validClassURI) {
-			throw new InvalidDynamicConceptException(
+			throw new InvalidDynamicOntologyException(
 					"Invalid Class fields values. ClassURI, classPrefixURI and classPrefixAlias values must"
 							+ " be a valid URI with one of the ontology prefixes");
 		}
 
 		if (!validPropertyURI) {
-			throw new InvalidDynamicConceptException(
+			throw new InvalidDynamicOntologyException(
 					"Invalid Class fields values. ClassURI, classPrefixURI and classPrefixAlias values must"
 							+ " be a valid URI with one of the ontology prefixes");
 		}
 
 		if (!validObjectTypeURI) {
-			throw new InvalidDynamicConceptException("Invalid Class fields values. propertyObjectTypeURI values must"
+			throw new InvalidDynamicOntologyException("Invalid Class fields values. propertyObjectTypeURI values must"
 					+ " be a valid URI with one of the ontology prefixes");
 		}
 
 		if (!(newConcept.getProperty_type().equals(PropertyType.DatatypeProperty.toString())
 				|| newConcept.getProperty_type().equals(PropertyType.ObjectProperty.toString()))) {
-			throw new InvalidDynamicConceptException("Invalid propertyObjectTypeURI value. propertyObjectTypeURI field"
+			throw new InvalidDynamicOntologyException("Invalid propertyObjectTypeURI value. propertyObjectTypeURI field"
 					+ " must have a value either \"DatatypeProperty\" or \"ObjectProperty\" ");
 		}
 
@@ -88,7 +88,7 @@ public class DynamicConceptsValidation {
 			}
 
 			if (!check) {
-				throw new InvalidDynamicConceptException(
+				throw new InvalidDynamicOntologyException(
 						"Invalid DataType for new added DataProperty. Check documentation for more information");
 			}
 		}
--- src/com/iotplatform/validations/DynamicOntologyRequestValidation.java
+++ src/com/iotplatform/validations/DynamicOntologyRequestValidation.java
@@ -1,5 +1,61 @@
 package com.iotplatform.validations;
 
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+
+import org.springframework.stereotype.Component;
+
+import com.iotplatform.ontology.Prefix;
+
+/*
+ * DynamicOntologyRequestValidation class is used to validate and parse dynamic ontology request body 
+ * to be used to after that to generate SPARQL-in-SQL query to add new concepts as turtles to
+ *  mainOntology of a specific application domain 
+ *  
+ * It checks that the request is valid by checking that the request body format is acceptable and
+ * checks that the new concept does not violate any constraints:
+ * 
+ * 1- to insert a new class :
+ * 
+ * 	 insert new ontology class : name(required), uri(required), prefix(required) , 
+ *	 uniqueIdentifier(not required) , listOfProperties(not required), superClassList(not required), 
+ *	 typeClassList(not required), RestrictionList (not required) 
+ * 
+ * 2- to insert a new property:
+ * 
+ * 	 name(required), prefix(required), multipleValues(required), unique(required), 
+ * 	 subjectClass(not required), propertyType(required), range(not required) 
+ * 
+ */
+
+@Component
 public class DynamicOntologyRequestValidation {
+
+	private static Hashtable<String, Prefix> htblPrefixes;
+
+	private static void validateDynamicOntologyClassRequest(LinkedHashMap<String, Object> htblRequestBody) {
+
+	}
+
+	/*
+	 * getPrefix is used to get Prefix enum that maps prefixAlias and it will
+	 * return null if the prefixAlias is not valid
+	 */
+	private static Prefix getPrefix(String prefixAlias) {
+		if (htblPrefixes == null) {
+			htblPrefixes = new Hashtable<>();
+
+			for (Prefix prefix : Prefix.values()) {
+				htblPrefixes.put(prefix.getPrefixName(), prefix);
+			}
+
+		}
+
+		if (htblPrefixes.containsKey(prefixAlias)) {
+			return htblPrefixes.get(prefixAlias);
+		} else {
+			return null;
+		}
+	}
 
 }
