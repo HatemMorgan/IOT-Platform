98acec7938dd20f111b683a0dffd21f0ca8158bc
Hatem Morgan <hatemmorgan17@gmail.com>
2017-05-03 16:05:10 +0200

    done with testing

--- src/com/iotplatform/configs/AppConfig.java
+++ src/com/iotplatform/configs/AppConfig.java
@@ -10,11 +10,4 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "com.iotplatform")
 public class AppConfig {
 
-	public static final HttpHeaders HTTP_HEADERS = new HttpHeaders();
-	static{
-	    HTTP_HEADERS.add("Access-Control-Allow-Headers", "Content-Type");
-	    HTTP_HEADERS.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-	    HTTP_HEADERS.add("Access-Control-Allow-Origin", "*");
-	}
-	
 }
